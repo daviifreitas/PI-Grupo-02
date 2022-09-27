da2dfce8c197aa22884333e485928c510a67cf92
Davi Freitas <davimello14@gmail.com>
2022-09-26 22:40:31 -0300

    Atualizações para migrações e implementação em banco de dados

--- PI/PI.UI/Program.cs
+++ PI/PI.UI/Program.cs
@@ -1,7 +1,11 @@
+using PI.Infra.IoC;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddInfrastructure();
 
 var app = builder.Build();
 
