540b07a8f765dbde0531658f6c98a7c45c144a08
Davi Freitas <davimello14@gmail.com>
2022-10-03 16:24:00 -0300

    simple changes

--- PI/PI.UI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PI.UI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
