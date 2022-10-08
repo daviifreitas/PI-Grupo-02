d9d09ef4d5c253fed7fc73d04eeeb8b8deb0f370
Thais Paes <thaisreginapaesdossantos@gmail.com>
2022-10-08 14:01:42 -0300

    Atualização da API
    ultima versão da API

--- IoT/Api/app.py
+++ IoT/Api/app.py
@@ -1,8 +1,11 @@
-from flask import Flask, json, request, jsonify
+from flask import Flask, json, request, jsonify, Response
 import pandas as pd
 
+
+# Ter o Arquivo dados_maq.csv baixado na sua pasta
+# Para utilizar é necessário informar no PATH_FILE o local que foi colocado o arquivo
 app = Flask(__name__)
-PATH_FILES = 'dados_maq.csv'
+PATH_FILE = 'dados_maq.csv'
 
 
 # Funcionalidades
@@ -11,16 +14,16 @@ def teste() -> str:
     return 'Testando Api'
 
 
-@app.route("/getCSV", methods=['GET'])
-def getCSV():
-    df = pd.read_csv(PATH_FILES)
-    result = df.to_json(orient="records")
-    dados = json.loads(result)
-    return json.dumps(dados)
+@app.route("/readCSV", methods=['GET'])
+def getCSV() -> str:
+    read = pd.read_csv(PATH_FILE)
+    dados = read.to_json(orient="records")
+    tabela = json.loads(dados)
+    return json.dumps(tabela)
 
 
 @app.route('/gravarDados', methods=['POST'])
-def postJson():
+def postJson() -> Response:
     dados = request.json
     with open("dados_maq.csv", "a") as arquivo:
         arquivo.write(str(dados["id"]) + "," + str(dados["temp"]) + "," +
@@ -29,5 +32,5 @@ def postJson():
         return jsonify(dados)
 
 
-# Api
+# Informando que a API vai estar no seu localhost
 app.run(host='0.0.0.0')
